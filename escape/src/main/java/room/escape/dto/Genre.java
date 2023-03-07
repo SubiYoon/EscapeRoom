8b4ebc2296022ba6da10c2d5418ee12e84b99d74
JizeroK <blopp54@naver.com>
2023-03-07 17:40:39 +0900

    지영 dto 커밋

---
+++ escape/src/main/java/room/escape/dto/Genre.java
@@ -0,0 +1,14 @@
+package room.escape.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
+public class Genre {
+    private int genre;
+    private String gname;
+
+}

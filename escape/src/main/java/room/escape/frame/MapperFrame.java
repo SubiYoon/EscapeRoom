bc1f0ccf096b40ad0bf6770a77134c2381c8855a
JizeroK <blopp54@naver.com>
2023-03-08 18:55:43 +0900

    지영 frame 수정

--- escape/src/main/java/room/escape/frame/MapperFrame.java
+++ escape/src/main/java/room/escape/frame/MapperFrame.java
@@ -3,5 +3,9 @@ package room.escape.frame;
 import java.util.List;
 
 public interface MapperFrame<K, V> {
+    public void insert(V v) throws Exception;
+    public void delete(K k) throws Exception;
+    public void update(V v) throws Exception;
+    public V select(K k) throws Exception;
     public List<V> selectAll() throws Exception;
 }
